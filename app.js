db404fa67ea65486524095a743f38a8d3b131b03
Erik Fjetland <efjetland@csumb.edu>
2017-11-29 03:19:17 +0000

    Added circle ci heroku deployment

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'Cookie Monster',
   resave: true,
+  saveUninitialized: false,
   cookie: {maxage: 60000}
 }));
 
