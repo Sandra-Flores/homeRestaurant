9d1f258ce4dc9d9527132caad19fd3b3031f56a2
Miguel Fletes <mfletes@csumb.edu>
2017-12-01 23:59:56 +0000

    finished login process

--- index.php
+++ index.php
@@ -34,10 +34,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="styles.css">
         <title>login</title>
     </head>
     <body>
+       <!-- <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId            : 'your-app-id',
+              autoLogAppEvents : true,
+              xfbml            : true,
+              version          : 'v2.11'
+            });
+          };
+        
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+        </script>-->
           <div>
               <form>
           
@@ -50,7 +68,6 @@
           
               </form>
           </div>
-          
     </body>
 </html>
    
