07b2996c4eb62fcfc6d75fda73012377eb1e8b48
Miguel Fletes <mfletes@csumb.edu>
2017-12-07 09:12:23 +0000

    connected the users.js to the users.jade file and edited the jade file to allow name input field

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  if(req.session.views) {
+        req.session.views++;
+    } else {
+        req.session.views = 1;
+    }
+    res.render('users', {title: "users", views: req.session.views});
 });
 
 module.exports = router;
