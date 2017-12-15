f2cc4919b3227fa86ff091d4f1d2cdfda2174749
Sandra Flores <sfloressilverio@csumb.edu>
2017-12-15 08:22:46 +0000

    Revert "Added functions to testing circleci"
    This reverts commit 94642ce6951ec60edf6059be33039c7d82bceef7.

--- test/test.js
+++ test/test.js
@@ -1,35 +1,21 @@
 var request = require("request");
-var expect = require("chai").expect;
 var helloWorld = require("../app.js")
 var base_url = "http://localhost:3000/"
 
-var index = require("../routes/index.js");
-var express = require('express');
+describe("Hello World Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
 
-describe("Testing home page", function() {
-    it("Home page should not be null", function(done) {
+    it("returns Hello World", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).not.to.be(null);
-        done();
-      });
-    });
-    
-describe("testing google map", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
+        expect(body).toBe("Hello World");
         done();
       });
     });
   });
- 
-describe("testing facebook login", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
-  });
-  
 }); 
