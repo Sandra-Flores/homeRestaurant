56603578e327881fecf8e5eaa03241a210f3d793
Sandra Flores <sfloressilverio@csumb.edu>
2017-12-15 08:00:13 +0000

    Testing v2

--- test/test.js
+++ test/test.js
@@ -10,25 +10,25 @@ var express = require('express');
 
 describe("Testing home page", function() {
     it("Home page should not be null", function(done) {
-      index.get(base_url, function(error, response, body) {
-        expect(body).to.be.null;
+      request.get(base_url, function(error, response, body) {
+        expect(body).to.not.be.null;
         // done();
       });
     });
     
 describe("Testing home page", function() {
     it("Home page should not be null", function(done) {
-      restaurant.get(base_url, function(error, response, body) {
-        expect(body).to.be.null;
-        // done();
+      request.get(base_url, function(error, response, body) {
+        expect(body).to.not.be.null;
+         done();
       });
     });
     
 describe("Testing restaurant page", function() {
     it("Restaurant page should not be null", function(done) {
-      restaurantSetUp.get(base_url, function(error, response, body) {
-        expect(body).to.be.null;
-        // done();
+      request.get(base_url, function(error, response, body) {
+        expect(body).to.not.be.null;
+        done();
       });
     });
     
