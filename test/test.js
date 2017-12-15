2ad5ca08010a1616ba91f44a8bc34886c62234ca
Sandra Flores <sfloressilverio@csumb.edu>
2017-12-15 07:52:59 +0000

    Added tests for main pages

--- test/test.js
+++ test/test.js
@@ -4,32 +4,35 @@ var helloWorld = require("../app.js")
 var base_url = "http://localhost:3000/"
 
 var index = require("../routes/index.js");
+var restaurant = require("../routes/restaurant.js");
+var restaurantSetUp = require("../routes/restaurantSetUp.js");
 var express = require('express');
 
 describe("Testing home page", function() {
     it("Home page should not be null", function(done) {
-      request.get(base_url, function(error, response, body) {
+      index.get(base_url, function(error, response, body) {
         expect(body).to.not.be.null;
         done();
       });
     });
     
-// describe("testing google map", function() {
-//     it("returns status code 200", function(done) {
-//       request.get(base_url, function(error, response, body) {
-//         expect(response.statusCode).toBe(200);
-//         done();
-//       });
-//     });
-//   });
- 
-// describe("testing facebook login", function() {
-//     it("returns status code 200", function(done) {
-//       request.get(base_url, function(error, response, body) {
-//         expect(response.statusCode).toBe(200);
-//         done();
-//       });
-//     });
-//   });
-  
-}); 
+describe("Testing home page", function() {
+    it("Home page should not be null", function(done) {
+      restaurant.get(base_url, function(error, response, body) {
+        expect(body).to.not.be.null;
+        done();
+      });
+    });
+    
+describe("Testing restaurant page", function() {
+    it("Restaurant page should not be null", function(done) {
+      restaurantSetUp.get(base_url, function(error, response, body) {
+        expect(body).to.not.be.null;
+        done();
+      });
+    });
+    
+    });
+    });
+});
+    
