c5de3ec56fe38bee26b5f09ef3a30798a929b63b
Sandra Flores <sfloressilverio@csumb.edu>
2017-12-16 02:29:52 +0000

    some tests

--- test/test.js
+++ test/test.js
@@ -1,21 +1,28 @@
 var request = require("request");
+var expect = require("chai").expect;
 var helloWorld = require("../app.js")
 var base_url = "http://localhost:3000/"
 
-describe("Hello World Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
+var index = require("../routes/index.js");
+var restaurant = require("../routes/restaurant.js");
+var restaurantSetUp = require("../routes/restaurantSetUp.js");
+var express = require('express');
+
+describe("Testing home page", function() {
+    it("Home page should not be null", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-        done();
+        expect(body).to.be.null;
+        // done();
       });
     });
 
-    it("returns Hello World", function(done) {
+describe("Testing home page", function() {
+    it("Home page should not be null", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
-        done();
+        expect(body).to.be.null;
+        // done();
       });
     });
-  });
-}); 
+
+    });
+});
